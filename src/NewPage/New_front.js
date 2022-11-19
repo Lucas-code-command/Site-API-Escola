2a2501bff279ba165af48bd5d3d2ce9071f2b37c
Lucas Moliterno <lucas.moliterno1@gmail.com>
2022-11-18 23:08:43 -0300

    .

--- src/NewPage/New_front.js
+++ src/NewPage/New_front.js
@@ -1,15 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import {Link} from 'react-router-dom';
 
 
 export default function New_front() {
     const [data,setData] = useState(null)
-    const [error, setError] = useState(null)
+    const[error, setError] = useState(null)
 
     useEffect(()=>{
         const getData = async() =>{
             try{
-                const response = await axios.get('http://localhost:5051/nba/times')
+                const response = await axios.get('http://localhost:5051/')
                 setData(response.data)
                 setError(null)
             }catch(err){
@@ -22,7 +23,8 @@ export default function New_front() {
     return(
 
         <div>
-            <li><Link to="/"> Home Page</Link></li>
+            <h1>worked??</h1>
+            {error && (<div>{`there problem ${error}`}</div>)}
             {data && data.map(({name})=>(
                 <h1>{name}</h1>
             ))}
